84d680b864f8609b063291d726a515170ff8e9cb
Harendra Shakya <harendrashakya2004@gmail.com>
2024-05-04 02:40:13 +0530

    create createCampaign function

--- packages/hardhat/contracts/CrowdFunding.sol
+++ packages/hardhat/contracts/CrowdFunding.sol
@@ -9,14 +9,48 @@ import "hardhat/console.sol";
 // - Implement chainlink CCIP for multichain transactions
 
 contract CrowdFunding {
+
+	error CrowdFunding__StartDate_ShouldBeInPresent();
+
 	// State Variables
+    uint256 private s_campaignsCount;
 
 	// Events: a way to emit log statements from smart contract that can be listened to by external parties
+
+    struct Campaign {
+        address payable creator;
+        uint256 id;
+        string name;
+        string description;
+        uint256 targetAmount;
+        uint256 amountCollected;
+        uint256 amountWithdrawnByOwner;
+    }
+
 
 	// Constructor: Called once on contract deployment
 	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
 
 	// Modifiers: used to define a set of rules that must be met before or after a function is executed
+
+	function createCampaign(
+        string memory _name,
+        string memory _description,
+        uint256 _targetAmount,
+        string memory _image
+    ) external returns (uint256) {
+
+        Campaign memory newCampaign = Campaign({
+            creator: payable(msg.sender),
+            id: s_campaignsCount,
+            name: _name,
+            description: _description,
+            targetAmount: _targetAmount,
+            amountCollected: 0,
+            amountWithdrawnByOwner: 0
+        });
+
+    }
 
 
 	/**
